b1063ee095ac00df133175b11221566402e5f417
Parminder Singh <shera1355@gmail.com>
2018-09-13 03:50:24 +0530

    registration intent added "manual2Input" registration speech actions and cases added error in "startActivity(manual2Input)" [App crashes] error in registerActivity.java Cannot find "contact" id of contact textview

--- app/src/main/java/com/example/parmindersingh/vmail/LoginActivity.java
+++ app/src/main/java/com/example/parmindersingh/vmail/LoginActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
