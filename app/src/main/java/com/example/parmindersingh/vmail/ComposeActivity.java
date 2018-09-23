1b40515c50488dfbc005c034a58b63477abddcaa
Antariksh C <antarikshc@gmail.com>
2018-09-23 12:56:47 +0530

    refactor: Try TapOutside instead of onClick

--- app/src/main/java/com/example/parmindersingh/vmail/ComposeActivity.java
+++ app/src/main/java/com/example/parmindersingh/vmail/ComposeActivity.java
@@ -99,8 +99,28 @@ public class ComposeActivity extends AppCompatActivity {
     }
 
     public void speech(View view){
-        layoutClicked();
+        //layoutClicked();
     }
+    
+    // Tap outside of components to remove focus
+  @Override
+  public boolean dispatchTouchEvent(MotionEvent event) {
+    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+      View v = getCurrentFocus();
+      if (v instanceof EditText) {
+        Rect outRect = new Rect();
+        v.getGlobalVisibleRect(outRect);
+        if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+          v.clearFocus();
+          InputMethodManager imm = (InputMethodManager) getSystemService(
+              Context.INPUT_METHOD_SERVICE);
+          imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            layoutClicked();
+        }
+      }
+    }
+    return super.dispatchTouchEvent(event);
+  }
 
     public void layoutClicked()
     {
